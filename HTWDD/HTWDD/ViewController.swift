99b73047e30c8aea5eda5b8647b041fc210325f3
Benjamin Herzog <mail@benchr.de>
2016-11-07 12:27:09 +0100

    Added Xcode project

---
+++ HTWDD/HTWDD/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HTWDD
+//
+//  Created by Benjamin Herzog on 07/11/2016.
+//  Copyright © 2016 HTW Dresden. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
