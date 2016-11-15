adbeff0789c44d825bdf1bf91bc95436bcd44428
Benjamin Herzog <mail@benchr.de>
2016-11-15 22:25:17 +0100

    Added a test back for testing. Changed travis yml

---
+++ HTWDDTests/HTWDDTest.swift
@@ -0,0 +1,18 @@
+//
+//  HTWDDTest.swift
+//  HTWDD
+//
+//  Created by Benjamin Herzog on 15/11/2016.
+//  Copyright © 2016 HTW Dresden. All rights reserved.
+//
+
+import XCTest
+
+class HTWDDTest: XCTestCase {
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+}
