5de841d0cbe8574575efb8491306fe7d77fba30d
Benjamin Herzog <mail@benchr.de>
2016-11-15 12:21:58 +0100

    Fail test

--- HTWDDTests/HTWDDTests.swift
+++ HTWDDTests/HTWDDTests.swift
@@ -24,6 +24,7 @@ class HTWDDTests: XCTestCase {
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+        XCTAssert(false)
     }
 
     func testPerformanceExample() {
