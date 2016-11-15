987222e7dac0fea33e71efd8fc08fb4ca036261d
Benjamin Herzog <mail@benchr.de>
2016-11-15 11:14:15 +0100

    Created project structure

---
+++ HTWDD/HTWDD/Core/Base Classes/ViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ViewController.swift
+//  HTWDD
+//
+//  Created by Benjamin Herzog on 15/11/2016.
+//  Copyright © 2016 HTW Dresden. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+}
--- HTWDD/HTWDD/Supporting Files/AppDelegate.swift
+++ HTWDD/HTWDD/Supporting Files/AppDelegate.swift
@@ -15,7 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         self.window = UIWindow()
-        self.window?.rootViewController = UIViewController()
+        self.window?.rootViewController = ViewController()
         self.window?.makeKeyAndVisible()
         return true
     }
