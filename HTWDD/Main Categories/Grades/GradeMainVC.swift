db6a645d952941fe98b83a47556a50540cf65410
Benjamin Herzog <mail@benchr.de>
2016-11-30 14:38:24 +0100

    Added GradeMainVC, synx

---
+++ HTWDD/Main Categories/Grades/GradeMainVC.swift
@@ -0,0 +1,19 @@
+//
+//  GradeMainVC.swift
+//  HTWDD
+//
+//  Created by Benjamin Herzog on 30/11/2016.
+//  Copyright © 2016 HTW Dresden. All rights reserved.
+//
+
+import UIKit
+
+class GradeMainVC: ViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
