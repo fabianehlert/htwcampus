c2ebc54600e1e8ad02c221e4b65368f4f85cc333
Benjamin Herzog <mail@benchr.de>
2017-01-04 18:01:02 +0100

    Added GradeModel

---
+++ HTWDD/Main Categories/Grades/GradeModel.swift
@@ -0,0 +1,26 @@
+//
+//  GradeModel.swift
+//  HTWDD
+//
+//  Created by Benjamin Herzog on 04/01/2017.
+//  Copyright © 2017 HTW Dresden. All rights reserved.
+//
+
+import Foundation
+
+protocol GradeSettings: class {
+    var sNumber: Setting<String> { get }
+    var unixPassword: Setting<String> { get }
+}
+
+extension SettingsManager: GradeSettings {}
+
+final class GradeModel {
+    
+    weak var settings: GradeSettings?
+    
+    init(settings: GradeSettings) {
+        self.settings = settings
+    }
+    
+}
