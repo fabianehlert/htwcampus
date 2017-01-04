526fdfa30c6e9f1a329941fbbe2040888cf79d45
Benjamin Herzog <mail@benchr.de>
2017-01-04 18:24:04 +0100

    Added unit tests

--- HTWDD/Supporting Files/AppDelegate.swift
+++ HTWDD/Supporting Files/AppDelegate.swift
@@ -15,7 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         baseInitialization()
-        
+
         self.window = UIWindow()
         self.window?.rootViewController = GradeMainVC().inNavigationController()
         self.window?.makeKeyAndVisible()
