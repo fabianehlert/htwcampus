c363912d5a36d2dc1e784c304e57763325046cf0
Benjamin Herzog <mail@benchr.de>
2017-01-05 18:10:38 +0100

    Moved Semester to own file, added tests for Grade

--- HTWDD/Main Categories/Grades/Grade.swift
+++ HTWDD/Main Categories/Grades/Grade.swift
@@ -11,86 +11,6 @@ import RxSwift
 
 struct Grade {
 
-    enum Semester: Hashable, CustomStringConvertible, Comparable {
-        case summer(year: Int)
-        case winter(year: Int)
-
-        init?(rawValue: String) {
-            guard rawValue.characters.count == 5 else {
-                return nil
-            }
-
-            var raw = rawValue
-
-            let type = raw.characters.removeLast()
-
-            guard let year = Int(raw), type == "1" || type == "2" else {
-                return nil
-            }
-            if type == "1" {
-                self = .summer(year: year)
-            } else if type == "2" {
-                self = .winter(year: year)
-            } else {
-                return nil
-            }
-        }
-
-        var year: Int {
-            switch self {
-            case .summer(let year):
-                return year
-            case .winter(let year):
-                return year
-            }
-        }
-
-        var description: String {
-            switch self {
-            case .summer(let year):
-                return "SS_\(year)"
-            case .winter(let year):
-                return "WS_\(year)"
-            }
-        }
-
-        var hashValue: Int {
-            return description.hashValue
-        }
-
-        static func <(lhs: Semester, rhs: Semester) -> Bool {
-            switch (lhs, rhs) {
-            case (.summer(let year1), .summer(let year2)):
-                return year1 < year2
-            case (.winter(let year1), .winter(let year2)):
-                return year1 < year2
-            case (.summer(let year1), .winter(let year2)):
-                if year1 == year2 {
-                    return true
-                } else {
-                    return year1 < year2
-                }
-            case (.winter(let year1), .summer(let year2)):
-                if year1 == year2 {
-                    return false
-                } else {
-                    return year1 < year2
-                }
-            }
-        }
-
-        static func ==(lhs: Semester, rhs: Semester) -> Bool {
-            switch (lhs, rhs) {
-            case (.summer(let year1), .summer(let year2)):
-                return year1 == year2
-            case (.winter(let year1), .winter(let year2)):
-                return year1 == year2
-            default:
-                return false
-            }
-        }
-    }
-
     var nr: Int
     var status: String
     var credits: Double
@@ -163,4 +83,15 @@ extension Grade: JSONInitializable {
         self.note = j["Vermerk"].flatMap { $0.isEmpty ? nil : $0 }
 
     }
+}
+
+extension Grade: Equatable {
+
+    static func ==(lhs: Grade, rhs: Grade) -> Bool {
+        return lhs.nr == rhs.nr
+            && lhs.mark == rhs.mark
+            && lhs.credits == rhs.credits
+            && lhs.semester == rhs.semester
+    }
+
 }
