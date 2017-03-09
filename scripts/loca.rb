5f8acc061600875ecac42ef3865e32f161cac2e0
Benjamin Herzog <mail@benchr.de>
2017-03-09 21:08:25 +0100

    Added localization files + code generation script

---
+++ scripts/loca.rb
@@ -0,0 +1,23 @@
+#!/usr/local/bin/ruby
+
+if ARGV[0] == nil
+	puts 'Specify input file!'
+	exit 1
+end
+
+puts "// This file is generated, do not change by hand!\n"
+puts "import Foundation\n\n"
+puts "enum Loca {\n"
+
+File.open(ARGV[0]).each do |line|
+
+	next if !/\".*\"\s*=\s*\".*\";/.match(line)
+
+	key = line[/\"(.*)\"\s*=\s*\".*\";/, 1]
+	value = line[/\".*\"\s*=\s*\"(.*)\";/, 1]
+
+	puts "\t// #{value}\n"
+	puts "\tvar #{key}: String { return NSLocalizedString(\"#{key}\", comment: \"\") }\n"
+end
+
+puts "}\n"
