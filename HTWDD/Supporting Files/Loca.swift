f001bdd3911ab211f6a52342ee0679ebcd4f0baf
Benjamin Herzog <mail@benchr.de>
2017-03-09 21:08:25 +0100

    Added localization files + code generation script

---
+++ HTWDD/Supporting Files/Loca.swift
@@ -0,0 +1,19 @@
+// This file is generated, do not change by hand!
+import Foundation
+
+enum Loca {
+	// Monday
+	var monday: String { return NSLocalizedString("monday", comment: "") }
+	// Tuesday
+	var tuesday: String { return NSLocalizedString("tuesday", comment: "") }
+	// Wednesday
+	var wednesday: String { return NSLocalizedString("wednesday", comment: "") }
+	// Thursday
+	var thursday: String { return NSLocalizedString("thursday", comment: "") }
+	// Friday
+	var friday: String { return NSLocalizedString("friday", comment: "") }
+	// Saturday
+	var saturday: String { return NSLocalizedString("saturday", comment: "") }
+	// Sunday
+	var sunday: String { return NSLocalizedString("sunday", comment: "") }
+}
