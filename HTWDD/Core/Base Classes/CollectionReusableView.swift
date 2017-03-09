3861b247bdd18efdd4133555c6b046ad7102b70c
Benjamin Herzog <mail@benchr.de>
2017-03-09 22:27:40 +0100

    Added base classes

---
+++ HTWDD/Core/Base Classes/CollectionReusableView.swift
@@ -0,0 +1,25 @@
+//
+//  CollectionReusableView.swift
+//  HTWDD
+//
+//  Created by Benjamin Herzog on 09/03/2017.
+//  Copyright © 2017 HTW Dresden. All rights reserved.
+//
+
+import UIKit
+
+class CollectionReusableView: UICollectionReusableView {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        initialSetup()
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        initialSetup()
+    }
+
+    func initialSetup() {}
+
+}
