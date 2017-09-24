d1afef9a08087cb21b1658f70191d73ac0f45ca7
Benjamin Herzog <mail@benchr.de>
2017-09-24 14:31:49 +0200

    Moved AppContext to own file

---
+++ HTWDD/Main Categories/General/AppContext.swift
@@ -0,0 +1,17 @@
+//
+//  AppContext.swift
+//  HTWDD
+//
+//  Created by Benjamin Herzog on 17.09.17.
+//  Copyright © 2017 HTW Dresden. All rights reserved.
+//
+
+import Foundation
+
+class User {
+    
+}
+
+class AppContext {
+    
+}
--- HTWDD/Main Categories/General/AppDelegate.swift
+++ HTWDD/Main Categories/General/AppDelegate.swift
@@ -8,10 +8,6 @@
 
 import UIKit
 
-class AppContext {
-
-}
-
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
