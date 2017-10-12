dad19f82143f1009d2d05e0944af41e7b9c5bef9
Fabian Ehlert <fabian@fabianehlert.com>
2017-10-12 16:25:53 +0200

    Adds OnboardStudygroupViewController.swift

---
+++ HTWDD/Onboarding/Steps/Studygroup/OnboardStudygroupViewController.swift
@@ -0,0 +1,23 @@
+//
+//  OnboardStudygroupViewController.swift
+//  HTWDD
+//
+//  Created by Fabian Ehlert on 12.10.17.
+//  Copyright © 2017 HTW Dresden. All rights reserved.
+//
+
+import UIKit
+
+class OnboardStudygroupViewController: UIViewController {
+
+	// MARK: - ViewController lifecycle
+	
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+}
