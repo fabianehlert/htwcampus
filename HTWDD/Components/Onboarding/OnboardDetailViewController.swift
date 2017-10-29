99c9edbc812b19e3235431423dbb6dde42f54afb
Benjamin Herzog <mail@benchr.de>
2017-10-29 14:16:12 +0100

    Added base class for onboarding view controller

---
+++ HTWDD/Components/Onboarding/OnboardDetailViewController.swift
@@ -0,0 +1,181 @@
+//
+//  OnboardDetailViewController.swift
+//  HTWDD
+//
+//  Created by Benjamin Herzog on 29.10.17.
+//  Copyright © 2017 HTW Dresden. All rights reserved.
+//
+
+import UIKit
+
+class OnboardDetailViewController<Product>: ViewController, UITextFieldDelegate {
+
+    var onFinish: ((Product?) -> Void)?
+
+    struct Config {
+        var title: String
+        var description: String
+        var textFields: [TextField]
+        var textFieldStackViewAxis: UILayoutConstraintAxis
+        var notNowText: String
+        var continueButtonText: String
+    }
+
+    /// Set this config before calling super.initialSetup!
+    var config: Config?
+
+    private lazy var continueButton = ReactiveButton()
+
+    // MARK: - ViewController lifecycle
+
+    override func initialSetup() {
+        super.initialSetup()
+
+        guard let config = self.config else {
+            preconditionFailure("Tried to use OnboardDetailViewController without config. Abort!")
+        }
+
+        self.navigationController?.interactivePopGestureRecognizer?.isEnabled = false
+
+        // --- Title Label ---
+
+        let titleLabel = UILabel()
+        titleLabel.text = config.title
+        titleLabel.font = .systemFont(ofSize: 44, weight: .bold)
+        titleLabel.textColor = UIColor.htw.textHeadline
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+
+        let titleContainer = UIView()
+        titleContainer.translatesAutoresizingMaskIntoConstraints = false
+        titleContainer.addSubview(titleLabel)
+        self.view.addSubview(titleContainer)
+
+        // --- Description Label ---
+
+        let descriptionLabel = UILabel()
+        descriptionLabel.text = config.description
+        descriptionLabel.font = .systemFont(ofSize: 17, weight: .medium)
+        descriptionLabel.textColor = UIColor.htw.textBody
+        descriptionLabel.numberOfLines = 0
+        descriptionLabel.translatesAutoresizingMaskIntoConstraints = false
+
+        // --- Text fields ---
+
+        let configureTextField: (UITextField) -> Void = {
+            $0.font = .systemFont(ofSize: 30, weight: .medium)
+            $0.backgroundColor = UIColor.htw.veryLightGrey
+            $0.textAlignment = .center
+            $0.keyboardType = .numberPad
+            $0.delegate = self
+            $0.addTarget(self, action: #selector(self.inputChanges(textField:)), for: .editingChanged)
+            $0.translatesAutoresizingMaskIntoConstraints = false
+        }
+
+        config.textFields
+            .forEach(configureTextField)
+
+        let textFieldStackView = UIStackView(arrangedSubviews: config.textFields)
+        textFieldStackView.axis = config.textFieldStackViewAxis
+        textFieldStackView.distribution = .fillEqually
+        textFieldStackView.spacing = 12
+        textFieldStackView.translatesAutoresizingMaskIntoConstraints = false
+
+        let centerStackView = UIStackView(arrangedSubviews: [
+            descriptionLabel,
+            textFieldStackView
+            ])
+        centerStackView.axis = .vertical
+        centerStackView.distribution = .fill
+        centerStackView.spacing = 40
+        centerStackView.translatesAutoresizingMaskIntoConstraints = false
+        self.view.addSubview(centerStackView)
+
+        // --- Continue Button ---
+
+        self.continueButton.isEnabled = false
+        self.continueButton.setTitle(config.continueButtonText, for: .normal)
+        self.continueButton.titleLabel?.font = .systemFont(ofSize: 20, weight: .medium)
+        self.continueButton.backgroundColor = UIColor.htw.blue
+        self.continueButton.layer.cornerRadius = 12
+        self.continueButton.translatesAutoresizingMaskIntoConstraints = false
+        self.continueButton.addTarget(self, action: #selector(continueBoarding), for: .touchUpInside)
+        self.view.addSubview(self.continueButton)
+
+        // --- Skip Button ---
+
+        let skip = ReactiveButton()
+        skip.setTitle(config.notNowText, for: .normal)
+        skip.setTitleColor(UIColor.htw.blue, for: .normal)
+        skip.titleLabel?.font = .systemFont(ofSize: 16, weight: .medium)
+        skip.translatesAutoresizingMaskIntoConstraints = false
+        skip.addTarget(self, action: #selector(skipBoarding), for: .touchUpInside)
+        titleContainer.addSubview(skip)
+
+        // --- Constraints ---
+
+        NSLayoutConstraint.activate([
+            titleContainer.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
+            titleContainer.bottomAnchor.constraint(equalTo: centerStackView.topAnchor),
+            titleContainer.widthAnchor.constraint(equalTo: centerStackView.widthAnchor),
+
+            skip.topAnchor.constraint(equalTo: titleContainer.topAnchor),
+            skip.trailingAnchor.constraint(equalTo: titleContainer.trailingAnchor),
+
+            titleLabel.centerXAnchor.constraint(equalTo: titleContainer.centerXAnchor),
+            titleLabel.centerYAnchor.constraint(equalTo: titleContainer.centerYAnchor),
+
+            centerStackView.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
+            centerStackView.centerYAnchor.constraint(equalTo: self.view.centerYAnchor, constant: -50),
+            centerStackView.widthAnchor.constraint(equalTo: self.view.widthAnchor, multiplier: 0.8),
+
+            textFieldStackView.heightAnchor.constraint(equalToConstant: 60),
+
+            self.continueButton.heightAnchor.constraint(equalToConstant: 55),
+            self.continueButton.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
+            self.continueButton.widthAnchor.constraint(equalTo: centerStackView.widthAnchor)
+            ])
+
+        var top = NSLayoutConstraint()
+        var bottom = NSLayoutConstraint()
+        if #available(iOS 11.0, *) {
+            top = titleContainer.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor, constant: 12)
+            bottom = self.continueButton.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor, constant: -20)
+        } else {
+            top = titleContainer.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 12)
+            bottom = self.continueButton.bottomAnchor.constraint(equalTo: self.view.bottomAnchor, constant: -20)
+        }
+        NSLayoutConstraint.activate([top, bottom])
+    }
+
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        return .portrait
+    }
+
+    @objc func continueBoarding() {
+        preconditionFailure("Overwrite this  method in your subclass!")
+    }
+
+    @objc func skipBoarding() {
+        self.onFinish?(nil)
+    }
+
+    func shouldReplace(textField: UITextField, newString: String) -> Bool {
+        return true
+    }
+
+    func shouldContinue(textField: UITextField) -> Bool {
+        return false
+    }
+
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        let currentString = (textField.text ?? "") as NSString
+        let newString = currentString.replacingCharacters(in: range, with: string) as String
+
+        return self.shouldReplace(textField: textField, newString: newString)
+    }
+
+    @objc func inputChanges(textField: TextField) {
+        self.continueButton.isEnabled = self.shouldContinue(textField: textField)
+    }
+
+}
--- HTWDD/Components/Onboarding/OnboardStudygroupViewController.swift
+++ HTWDD/Components/Onboarding/OnboardStudygroupViewController.swift
@@ -8,229 +8,84 @@
 
 import UIKit
 
-class OnboardStudygroupViewController: ViewController {
+class OnboardStudygroupViewController: OnboardDetailViewController<ScheduleService.Auth> {
 
-	var onContinue: ((OnboardStudygroupViewController, ScheduleService.Auth?) -> Void)?
-	var onSkip: ((OnboardStudygroupViewController) -> Void)?
+    private lazy var yearTextField = StudygroupTextField()
+    private lazy var majorTextField = StudygroupTextField()
+    private lazy var groupTextField = StudygroupTextField()
 
-	private lazy var continueButton = ReactiveButton()
+    // MARK: - ViewController lifecycle
 
-	private lazy var yearTextField = StudygroupTextField()
-	private lazy var majorTextField = StudygroupTextField()
-	private lazy var groupTextField = StudygroupTextField()
+    override func initialSetup() {
+        self.config = .init(title: Loca.Onboarding.Studygroup.title,
+                            description: Loca.Onboarding.Studygroup.body,
+                            textFields: [self.yearTextField, self.majorTextField, self.groupTextField],
+                            textFieldStackViewAxis: UILayoutConstraintAxis.horizontal,
+                            notNowText: Loca.Onboarding.Studygroup.notnow,
+                            continueButtonText: Loca.nextStep)
 
-	// MARK: - ViewController lifecycle
+        self.yearTextField.configurationType = .year
+        self.majorTextField.configurationType = .major
+        self.groupTextField.configurationType = .group
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+        super.initialSetup()
+
+        // --- Make first text field active ---
+        self.yearTextField.becomeFirstResponder()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
+    // MARK: - Actions
+
+    @objc
+    override func continueBoarding() {
+        guard
+            let y = self.yearTextField.text,
+            let m = self.majorTextField.text,
+            let g = self.groupTextField.text
+        else {
+                self.onFinish?(nil)
+                return
+        }
+
+        let group = ScheduleService.Auth(year: y, major: m, group: g)
+        self.onFinish?(group)
     }
 
-	override func initialSetup() {
-		self.navigationController?.interactivePopGestureRecognizer?.isEnabled = false
+    override func shouldReplace(textField: UITextField, newString: String) -> Bool {
+        guard
+            let f = textField as? StudygroupTextField,
+            let config = f.configurationType
+        else {
+                return super.shouldReplace(textField: textField, newString: newString)
+        }
 
-		// --- Title Label ---
+        if newString.count > config.length {
+            return false
+        }
 
-		let titleLabel = UILabel()
-		titleLabel.text = Loca.Onboarding.Studygroup.title
-		titleLabel.font = .systemFont(ofSize: 44, weight: .bold)
-		titleLabel.textColor = UIColor.htw.textHeadline
-		titleLabel.translatesAutoresizingMaskIntoConstraints = false
-
-		let titleContainer = UIView()
-		titleContainer.translatesAutoresizingMaskIntoConstraints = false
-		titleContainer.addSubview(titleLabel)
-		self.view.addSubview(titleContainer)
-
-		// --- Description Label ---
-
-		let descriptionLabel = UILabel()
-		descriptionLabel.text = Loca.Onboarding.Studygroup.body
-		descriptionLabel.font = .systemFont(ofSize: 17, weight: .medium)
-		descriptionLabel.textColor = UIColor.htw.textBody
-		descriptionLabel.numberOfLines = 0
-		descriptionLabel.translatesAutoresizingMaskIntoConstraints = false
-
-		// --- Text fields ---
-
-		self.yearTextField.configurationType = .year
-		self.yearTextField.font = .systemFont(ofSize: 30, weight: .medium)
-		self.yearTextField.backgroundColor = UIColor.htw.veryLightGrey
-		self.yearTextField.textAlignment = .center
-		self.yearTextField.keyboardType = .numberPad
-		self.yearTextField.delegate = self
-		self.yearTextField.addTarget(self, action: #selector(inputChanges(textField:)), for: .editingChanged)
-		self.yearTextField.translatesAutoresizingMaskIntoConstraints = false
-
-		self.majorTextField.configurationType = .major
-		self.majorTextField.font = .systemFont(ofSize: 30, weight: .medium)
-		self.majorTextField.backgroundColor = UIColor.htw.veryLightGrey
-		self.majorTextField.textAlignment = .center
-		self.majorTextField.keyboardType = .numberPad
-		self.majorTextField.delegate = self
-		self.majorTextField.addTarget(self, action: #selector(inputChanges(textField:)), for: .editingChanged)
-		self.majorTextField.translatesAutoresizingMaskIntoConstraints = false
-
-		self.groupTextField.configurationType = .group
-		self.groupTextField.font = .systemFont(ofSize: 30, weight: .medium)
-		self.groupTextField.backgroundColor = UIColor.htw.veryLightGrey
-		self.groupTextField.textAlignment = .center
-		self.groupTextField.keyboardType = .numberPad
-		self.groupTextField.delegate = self
-		self.groupTextField.addTarget(self, action: #selector(inputChanges(textField:)), for: .editingChanged)
-		self.groupTextField.translatesAutoresizingMaskIntoConstraints = false
-
-		let textFieldStackView = UIStackView(arrangedSubviews: [
-			self.yearTextField,
-			self.majorTextField,
-			self.groupTextField
-			])
-		textFieldStackView.axis = .horizontal
-		textFieldStackView.distribution = .fillEqually
-		textFieldStackView.spacing = 12
-		textFieldStackView.translatesAutoresizingMaskIntoConstraints = false
-
-		let centerStackView = UIStackView(arrangedSubviews: [
-			descriptionLabel,
-			textFieldStackView
-			])
-		centerStackView.axis = .vertical
-		centerStackView.distribution = .fill
-		centerStackView.spacing = 40
-		centerStackView.translatesAutoresizingMaskIntoConstraints = false
-		self.view.addSubview(centerStackView)
-
-		// --- Continue Button ---
-
-		self.continueButton.isEnabled = false
-		self.continueButton.setTitle(Loca.letsgo, for: .normal)
-		self.continueButton.titleLabel?.font = .systemFont(ofSize: 20, weight: .medium)
-		self.continueButton.backgroundColor = UIColor.htw.blue
-		self.continueButton.layer.cornerRadius = 12
-		self.continueButton.translatesAutoresizingMaskIntoConstraints = false
-		self.continueButton.addTarget(self, action: #selector(continueBoarding), for: .touchUpInside)
-		self.view.addSubview(self.continueButton)
-
-		// --- Skip Button ---
-
-		let skip = ReactiveButton()
-		skip.setTitle(Loca.Onboarding.Studygroup.notnow, for: .normal)
-		skip.setTitleColor(UIColor.htw.blue, for: .normal)
-		skip.titleLabel?.font = .systemFont(ofSize: 16, weight: .medium)
-		skip.translatesAutoresizingMaskIntoConstraints = false
-		skip.addTarget(self, action: #selector(skipBoarding), for: .touchUpInside)
-		titleContainer.addSubview(skip)
-
-		// --- Constraints ---
-
-		NSLayoutConstraint.activate([
-			titleContainer.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
-			titleContainer.bottomAnchor.constraint(equalTo: centerStackView.topAnchor),
-			titleContainer.widthAnchor.constraint(equalTo: centerStackView.widthAnchor),
-
-			skip.topAnchor.constraint(equalTo: titleContainer.topAnchor),
-			skip.trailingAnchor.constraint(equalTo: titleContainer.trailingAnchor),
-
-			titleLabel.centerXAnchor.constraint(equalTo: titleContainer.centerXAnchor),
-			titleLabel.centerYAnchor.constraint(equalTo: titleContainer.centerYAnchor),
-
-			centerStackView.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
-			centerStackView.centerYAnchor.constraint(equalTo: self.view.centerYAnchor, constant: -50),
-			centerStackView.widthAnchor.constraint(equalTo: self.view.widthAnchor, multiplier: 0.8),
-
-			textFieldStackView.heightAnchor.constraint(equalToConstant: 60),
-
-			self.continueButton.heightAnchor.constraint(equalToConstant: 55),
-			self.continueButton.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
-			self.continueButton.widthAnchor.constraint(equalTo: centerStackView.widthAnchor)
-			])
-
-		var top = NSLayoutConstraint()
-		var bottom = NSLayoutConstraint()
-		if #available(iOS 11.0, *) {
-			top = titleContainer.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor, constant: 12)
-			bottom = self.continueButton.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor, constant: -20)
-		} else {
-			top = titleContainer.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 12)
-			bottom = self.continueButton.bottomAnchor.constraint(equalTo: self.view.bottomAnchor, constant: -20)
-		}
-		NSLayoutConstraint.activate([top, bottom])
-
-		// --- Make first text field active ---
-		self.yearTextField.becomeFirstResponder()
-	}
-
-    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
-        return .portrait
+        return f.isInputValid(newString)
     }
 
-	// MARK: - Actions
+    override func shouldContinue(textField: UITextField) -> Bool {
+        if let textField = textField as? StudygroupTextField, textField.isInputFinal() {
+            self.jumpToNextField(from: textField)
+        }
 
-	@objc private func continueBoarding() {
-		if let y = self.yearTextField.text,
-			let m = self.majorTextField.text,
-			let g = self.groupTextField.text {
+        let y = yearTextField.isInputFinal()
+        let m = majorTextField.isInputFinal()
+        let g = groupTextField.isInputFinal()
 
-			let group = ScheduleService.Auth(year: y, major: m, group: g)
-			self.onContinue?(self, group)
-		} else {
-			self.onContinue?(self, nil)
-		}
-	}
+        return y && m && g
+    }
 
-	@objc private func skipBoarding() {
-		self.onSkip?(self)
-	}
-}
+    private func jumpToNextField(from textField: StudygroupTextField) {
+        if textField == self.yearTextField {
+            self.majorTextField.becomeFirstResponder()
+        } else if textField == self.majorTextField {
+            self.groupTextField.becomeFirstResponder()
+        } else if textField == self.groupTextField {
+            self.groupTextField.resignFirstResponder()
+        }
+    }
 
-// MARK: - UITextFieldDelegate
-extension OnboardStudygroupViewController: UITextFieldDelegate {
-	func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
-
-		// TODO: overthink this design
-
-		if let f = textField as? StudygroupTextField,
-			let config = f.configurationType {
-
-			let newString = (f.text ?? "") + string
-
-			if newString.count > config.length {
-				return false
-			}
-
-			return f.isInputValid(string)
-		}
-
-		return true
-	}
-
-	@objc func inputChanges(textField: StudygroupTextField) {
-		if textField.isInputFinal() {
-			self.jumpToNextField(from: textField)
-		}
-
-		let y = yearTextField.isInputFinal()
-		let m = majorTextField.isInputFinal()
-		let g = groupTextField.isInputFinal()
-
-		if y && m && g {
-			self.continueButton.isEnabled = true
-		} else {
-			self.continueButton.isEnabled = false
-		}
-	}
-
-	///
-	private func jumpToNextField(from textField: StudygroupTextField) {
-		if textField == self.yearTextField {
-			self.majorTextField.becomeFirstResponder()
-		} else if textField == self.majorTextField {
-			self.groupTextField.becomeFirstResponder()
-		} else if textField == self.groupTextField {
-			self.groupTextField.resignFirstResponder()
-		}
-	}
 }
