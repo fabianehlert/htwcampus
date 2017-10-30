8f208e691bd3f74cf8f6af302ebc5df3f14b96ed
Benjamin Herzog <mail@benchr.de>
2017-10-30 21:12:19 +0100

    Implement PersistenceService logic

---
+++ HTWDD/Core/GlobalFunctions.swift
@@ -0,0 +1,13 @@
+//
+//  GlobalFunctions.swift
+//  HTWDD
+//
+//  Created by Benjamin Herzog on 30.10.17.
+//  Copyright © 2017 HTW Dresden. All rights reserved.
+//
+
+import Foundation
+
+func identity<T>(_ v: T) -> T {
+    return v
+}
--- HTWDD/Main Categories/General/PersistenceService.swift
+++ HTWDD/Main Categories/General/PersistenceService.swift
@@ -12,15 +12,34 @@ import KeychainAccess
 
 class PersistenceService: Service {
 
-    struct Response {
-        var schedule: ScheduleService.Auth
-        var grades: GradeService.Auth
+    private enum Const {
+        static let appGroup = "group.htw-dresden.ios"
+
+        static let scheduleKey = "htw-dresden.schedule.auth"
+        static let gradesKey = "htw-dresden.grades.auth"
     }
-    
-    private let keychain = Keychain(accessGroup: <#T##String#>)
+
+    struct Response {
+        var schedule: ScheduleService.Auth?
+        var grades: GradeService.Auth?
+    }
+
+    private let keychain = Keychain(accessGroup: Const.appGroup)
 
     func load(parameters: () = ()) -> Observable<Response> {
-        
+        let res = Response(schedule: self.load(type: ScheduleService.Auth.self, key: Const.scheduleKey),
+                           grades: self.load(type: GradeService.Auth.self, key: Const.gradesKey))
+        return Observable.just(res)
+    }
+
+    private func load<T: Codable>(type: T.Type, key: String) -> T? {
+        let savedData = (try? keychain.getData(key)).flatMap(identity)
+
+        guard let data = savedData else {
+            return nil
+        }
+
+        return try? JSONDecoder().decode(T.self, from: data)
     }
 
 }
