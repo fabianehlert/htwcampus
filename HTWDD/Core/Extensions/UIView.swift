6d73e62110a9dc5df99d81762365c96dbd703bb6
Benjamin Herzog <mail@benchr.de>
2017-11-01 08:09:51 +0100

    Added handy view extensions

---
+++ HTWDD/Core/Extensions/UIView.swift
@@ -0,0 +1,43 @@
+//
+//  UIView.swift
+//  HTWDD
+//
+//  Created by Benjamin Herzog on 01.11.17.
+//  Copyright © 2017 HTW Dresden. All rights reserved.
+//
+
+import UIKit
+
+extension UIView {
+
+    var width: CGFloat {
+        get { return self.bounds.size.width }
+        set { self.bounds.size.width = newValue }
+    }
+
+    var height: CGFloat {
+        get { return self.bounds.size.height }
+        set { self.bounds.size.height = newValue }
+    }
+
+    var top: CGFloat {
+        get { return self.frame.origin.y }
+        set { self.frame.origin.y = newValue }
+    }
+
+    var bottom: CGFloat {
+        get { return self.top + self.height }
+        set { self.top = newValue - self.height }
+    }
+
+    var left: CGFloat {
+        get { return self.frame.origin.x }
+        set { self.frame.origin.x = newValue }
+    }
+
+    var right: CGFloat {
+        get { return self.left + self.width }
+        set { self.left = newValue - self.width }
+    }
+
+}
