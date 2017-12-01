76319e5e82a8f9857028d05b80d2257afc9d1a00
Fabian Ehlert <fabian@fabianehlert.com>
2017-12-01 17:00:41 +0100

    Adds KeyboardPresentationHandler

---
+++ HTWDD/Core/UI/KeyboardPresentationHandler.swift
@@ -0,0 +1,56 @@
+//
+//  KeyboardPresentationHandler.swift
+//  HTWDD
+//
+//  Created by Fabian Ehlert on 01.12.17.
+//  Copyright © 2017 HTW Dresden. All rights reserved.
+//
+
+import UIKit
+
+@objc
+protocol KeyboardPresentationHandler {
+	@objc
+	func keyboardWillShow(_ notification: Notification)
+	@objc
+	func keyboardWillHide(_ notification: Notification)
+}
+
+extension KeyboardPresentationHandler {
+	func registerForKeyboardNotifications() {
+		NotificationCenter.default.addObserver(self,
+											   selector: #selector(keyboardWillShow(_:)),
+											   name: .UIKeyboardWillShow,
+											   object: nil)
+		
+		NotificationCenter.default.addObserver(self,
+											   selector: #selector(keyboardWillHide(_:)),
+											   name: .UIKeyboardWillHide,
+											   object: nil)
+	}
+	
+	func handleKeyboardNotification(_ notification: Notification, handler: (TimeInterval, UIViewAnimationCurve, CGRect) -> Void) {
+		guard
+			let duration = notification.userInfo?[UIKeyboardAnimationDurationUserInfoKey] as? TimeInterval,
+			let curveValue = notification.userInfo?[UIKeyboardAnimationCurveUserInfoKey] as? Int,
+			let frame = notification.userInfo?[UIKeyboardFrameEndUserInfoKey] as? CGRect
+			else {
+				return
+		}
+		// the animation curve may be undocumented
+		var animationCurve = UIViewAnimationCurve.easeOut
+		NSNumber(value: curveValue).getValue(&animationCurve)
+		
+		handler(duration, animationCurve, frame)
+	}
+	
+	func animateWithKeyboardNotification(_ notification: Notification, layout: ((CGRect) -> Void)? = nil, animations: @escaping () -> Void) {
+		self.handleKeyboardNotification(notification) { duration, curve, rect in
+			layout?(rect)
+			UIView.animate(withDuration: duration, delay: 0, options: [.beginFromCurrentState, .allowUserInteraction], animations: {
+				UIView.setAnimationCurve(curve)
+				animations()
+			}, completion: nil)
+		}
+	}
+}
