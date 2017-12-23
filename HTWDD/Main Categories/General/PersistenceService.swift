a5b8646d393263ccd7a387e4e9a24df921c72a88
Fabian Ehlert <fabian@fabianehlert.com>
2017-12-23 19:25:00 +0100

    Adds load, save and remove funcs for “hidden”

--- HTWDD/Main Categories/General/PersistenceService.swift
+++ HTWDD/Main Categories/General/PersistenceService.swift
@@ -17,6 +17,7 @@ class PersistenceService: Service {
 		static let accessGroup = "3E4PGPNR47.keychain-group"
 		
         static let scheduleKey = "htw-dresden.schedule.auth"
+        static let scheduleHiddenKey = "htw-dresden.schedule.hidden"
         static let scheduleColorsKey = "htw-dresden.schedule.colors"
         static let gradesKey = "htw-dresden.grades.auth"
         
@@ -50,6 +51,10 @@ class PersistenceService: Service {
         return try? JSONDecoder().decode(T.self, from: data)
     }
     
+    func loadHidden() -> [Int: Bool] {
+        return self.load(type: [Int: Bool].self, key: Const.scheduleHiddenKey) ?? [:]
+    }
+    
     func loadScheduleColors() -> [Int: UInt] {
         return self.load(type: [Int: UInt].self, key: Const.scheduleColorsKey) ?? [:]
     }
@@ -79,6 +84,14 @@ class PersistenceService: Service {
 
     func save(_ schedule: ScheduleService.Auth) {
         self.save(object: schedule, key: Const.scheduleKey)
+    }
+    
+    func save(_ hidden: [Int: Bool]) {
+        var all = self.loadHidden()
+        hidden.forEach { key, value in
+            all[key] = value
+        }
+        self.save(object: all, key: Const.scheduleHiddenKey)
     }
     
     func save(_ colors: [Int: UInt]) {
@@ -116,6 +129,7 @@ class PersistenceService: Service {
     func clear() {
         self.removeGrade()
         self.removeSchedule()
+        self.removeScheduleHidden()
         self.removeScheduleColors()
         self.removeGradesCache()
         self.removeScheduleCache()
@@ -123,6 +137,10 @@ class PersistenceService: Service {
 
     func removeSchedule() {
         self.remove(key: Const.scheduleKey)
+    }
+    
+    func removeScheduleHidden() {
+        self.remove(key: Const.scheduleHiddenKey)
     }
     
     func removeScheduleColors() {
